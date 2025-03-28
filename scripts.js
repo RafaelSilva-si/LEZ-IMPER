5b1babadf91bb321aa34cb0fb8bce0602a5472c1
Rafael Silva <rafaelkaperski@gmail.com>
2025-03-28 10:32:27 -0300

    feat: remove css and js not used

--- scripts.js
+++ scripts.js
@@ -1,10 +1,3 @@
-window.onload = function() {
-    setTimeout(() => {
-        const preloader = document.getElementById('preloader');
-        preloader.style.display = 'none';
-    }, 1000);
-};
-
 function toggleAnswer(id) {
     var answer = document.getElementById("answer" + id);
     if (answer.style.display === "block") {
